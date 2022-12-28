5890985c4e3835d63a1cd33a646df79967e2d92d
kauan777 <kauan7622@gmail.com>
2022-12-28 14:56:25 -0300

    feat: creating class Book with getter and setters

---
+++ src/app/entities/book.ts
@@ -0,0 +1,58 @@
+import { randomUUID } from 'crypto';
+
+export interface BookProps {
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  quantity: number;
+}
+
+export class Book {
+  private _id: string;
+  private props: BookProps;
+
+  constructor(props: BookProps, id?: string) {
+    this._id = id ?? randomUUID();
+    this.props = props;
+  }
+
+  public get id(): string {
+    return this._id;
+  }
+
+  public get title(): string {
+    return this.props.title;
+  }
+  public set title(title: string) {
+    this.props.title = title;
+  }
+
+  public get description(): string {
+    return this.props.description;
+  }
+  public set description(description: string) {
+    this.props.description = description;
+  }
+
+  public get price(): number {
+    return this.props.price;
+  }
+  public set price(price: number) {
+    this.props.price = price;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get quantity(): number {
+    return this.props.quantity;
+  }
+  public set quantity(quantity: number) {
+    this.props.quantity = quantity;
+  }
+}
