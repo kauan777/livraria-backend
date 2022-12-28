37269c83c2dd40c28ecba735a6491d50291371b6
kauan777 <kauan7622@gmail.com>
2022-12-28 15:04:40 -0300

    feat: creating mapper to controller book

---
+++ src/infra/http/view-models/book-view-model.ts
@@ -0,0 +1,14 @@
+import { Book } from 'src/app/entities/book';
+
+export class BookViewModel {
+  static toHTTP(book: Book) {
+    return {
+      id: book.id,
+      title: book.title,
+      description: book.description,
+      category: book.category,
+      price: book.price,
+      quantity: book.quantity,
+    };
+  }
+}
