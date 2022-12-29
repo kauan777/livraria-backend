db4ba9241c5993c65ea958a2aca8f1d30447b6e9
kauan777 <kauan7622@gmail.com>
2022-12-29 09:12:47 -0300

    refactor: including orderModule

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { BookModule } from './book.module';
+import { OrderModule } from './order.module';
 
 @Module({
-  imports: [DatabaseModule, BookModule],
+  imports: [DatabaseModule, BookModule, OrderModule],
 })
 export class HttpModule {}
