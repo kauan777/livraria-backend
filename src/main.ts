274ce6a3a2dccc440f14b07b6a6a04c3c3eb18f6
kauan777 <kauan7622@gmail.com>
2022-12-29 10:54:23 -0300

    fix: adding cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './infra/app.module';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
