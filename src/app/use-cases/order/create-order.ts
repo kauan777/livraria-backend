123677b90432f998f5bf6b3a661e62bf400abde4
kauan777 <kauan7622@gmail.com>
2022-12-29 09:02:46 -0300

    refactor: removing status

--- src/app/use-cases/order/create-order.ts
+++ src/app/use-cases/order/create-order.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Order, StatusOrderProps } from 'src/app/entities/order';
+import { Order } from 'src/app/entities/order';
 import { OrdersRepository } from 'src/app/repositories/order-repository';
 
 interface CreateOrderRequest {
   countItems: number;
   emailCustomer: string;
   total: number;
-  status: StatusOrderProps;
 }
 
 interface CreateOrderResponse {
@@ -18,12 +17,11 @@ export class CreateOrder {
   constructor(private orderRepository: OrdersRepository) {}
 
   async execute(request: CreateOrderRequest): Promise<CreateOrderResponse> {
-    const { countItems, emailCustomer, status, total } = request;
+    const { countItems, emailCustomer, total } = request;
 
     const order = new Order({
       countItems,
       emailCustomer,
-      status,
       total,
     });
 
