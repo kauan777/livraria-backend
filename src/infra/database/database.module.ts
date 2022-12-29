b70fb5d5a492383fb8473004a7db5f790ae9f8fb
kauan777 <kauan7622@gmail.com>
2022-12-29 09:08:41 -0300

    feat: adding order repository

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaBooksRepository } from './prisma/repositories/prisma-books-repository';
 import { BooksRepository } from 'src/app/repositories/book-repository';
+import { OrdersRepository } from 'src/app/repositories/order-repository';
+import { PrismaOrdersRepository } from './prisma/repositories/prisma-orders-repository';
 
 @Module({
   providers: [
@@ -10,7 +12,11 @@ import { BooksRepository } from 'src/app/repositories/book-repository';
       provide: BooksRepository,
       useClass: PrismaBooksRepository,
     },
+    {
+      provide: OrdersRepository,
+      useClass: PrismaOrdersRepository,
+    },
   ],
-  exports: [BooksRepository],
+  exports: [BooksRepository, OrdersRepository],
 })
 export class DatabaseModule {}
